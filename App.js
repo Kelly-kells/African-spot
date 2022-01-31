bfeb7f70f57a0e887cff161bde74c0217b5cb3ad
unknown <kellskelechi@gmail.com>
2022-01-31 01:21:38 +0100

    header-section-ends

---
+++ App.js
@@ -0,0 +1,27 @@
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItems.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItems.classList.remove('active');
+}
+
+
+let cartItems = document.querySelector('.cart-items-container');
+document.querySelector('#cart-btn').onclick = () =>{
+    cartItems.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItems.classList.remove('active');
+}
